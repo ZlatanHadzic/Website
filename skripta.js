044259d6ca4f34ff8875e22442c0608e224d2a4f
ZlatanHadzic <128612411+ZlatanHadzic@users.noreply.github.com>
2024-03-14 12:14:41 +0100

    Update skripta.js

--- skripta.js
+++ skripta.js
@@ -28,7 +28,28 @@ const translations = {
     "about-link": "O nama",
     "service-link": "Usluge",
     "contact-link": "Kontakt",
-    "kredit": "Krerirao ZH-Media | sva prava zadržana!"
+    "kredit": "Krerirao ZH-Media | sva prava zadržana!",
+    "about-content": "Naša stručnost obuhvaća široki spektar područja, s fokusom na sistemsku i mrežnu integraciju, cybersecurity rješenja te unified communication and collaboration sustave.Kroz projektiranje infrastrukturnih rješenja, prilagođavamo se specifičnim zahtjevima poslovanja klijenata, osiguravajući da IT sustavi podržavaju i potiču poslovne ciljeve. Naša stručnost u brownfield implementacijama omogućava optimizaciju postojećih IT sustava, poboljšavajući njihovu učinkovitost i skalabilnost. ",
+    "Onama": "O nama",
+    "heading": "O nama",
+    "heading p": "Access Solutions je obrtničko društvo s posebnim naglaskom na pružanju visokokvalitetnih usluga u području savjetovanja i implementacije raznovrsnih rješenja",
+    "tko": "Ko smo mi?",
+    "dugmic": "Saznaj više",
+    "usluge": "Naše usluge",
+    "tekstic1": "ICT Consulting, stručno vođenje organizacija kroz primjenu informacijskih tehnologija kako bi unaprijedile poslovanje i postigle ciljeve. Pružamo prilagođene IT strategije i rješenja koja potiču inovacije i optimiziraju tehnološke resurse.",
+    "tekstic2": "Sistemske i mrežne integracije objedinjuju računalne sustave i mrežne komponente za optimizaciju tehnološke infrastrukture organizacije. Ovaj pristup poboljšava funkcionalnost, komunikaciju i ukupnu učinkovitost.",
+    "tekstic3": "Cybersecurity se bavi zaštitom informacijskih sustava od cyber prijetnji, uključujući zaštitu podataka, mreža i računalnih resursa od neovlaštenog pristupa, napada ili oštećenja. Osnovni cilj je očuvanje sigurnosti i integriteta digitalnih informacija.",
+    "tekstic4": " Jedinstvena komunikacija i suradnja odnosi se na usklađeni pristup poticanju učinkovite komunikacije i suradnje unutar organizacije. To uključuje integraciju različitih alata za komunikaciju i platformi za suradnju kako bi se poboljšala povezanost, olakšali radni procesi te potaknulo jedinstveno radno okruženje.",
+    "tekstic5": "Infrastrukturna rješenja obuhvaćaju implementaciju tehnološke infrastrukture, uključujući mreže, servere, sustave za pohranu podataka te ostale ključne komponente. Cilj im je podržati poslovne potrebe organizacije kroz pouzdane i učinkovite tehnološke resurse.",
+    "tekstic6": "Optimizacija postojećih IT sustava odnosi se na prilagodbu i poboljšanje postojeće informacijske tehnologije kako bi se postigla veća učinkovitost, poboljšala performansa i smanjili troškovi. Ovaj proces uključuje analizu, identifikaciju potencijalnih poboljšanja te implementaciju strategija kako bi se maksimalno iskoristili resursi i poboljšao ukupan rad IT sustava organizacije.",
+    "contact-infor": "Kontakt informacije",
+    "company": "Access Solutions je obrtničko društvo koje se bavi pružanjem usluga iz domena savjetovanja i implementacije rješenja iz domena sistemskih i mrežnih integracija, cybersecurity rješenja, kao i unified communication and collaboration sistema. Pored toga, bavimo se i projektovanjem infrastrukutralnih rješenja, te optimizacijom postojećih IT sistema, uz greenfield i brownfield implementacije.",
+    "futer1": "O nama",
+    "futer2": "Usluge",
+    "futer3": "Kontakt",
+    "futer4": "Pomoć",
+    "linkovi": "Linkovi",
+    "futer5": "Kontakt"
     },
     en: {
        "home-heading": "Why Waste Time? Leave It to Professionals!",
@@ -60,6 +81,27 @@ const translations = {
     "service-link": "Service",
     "contact-link": "Contact",
     "kredit": "Created by ZH-Media | all rights reserved!",
+    "about-content": "Our expertise encompasses a wide range of areas, with a focus on systemic and network integration, cybersecurity solutions, and unified communication and collaboration systems. Through the design of infrastructure solutions, we adapt to the specific business requirements of our clients, ensuring that IT systems support and drive business objectives. Our proficiency in brownfield implementations enables the optimization of existing IT systems, improving their efficiency and scalability.",
+    "Onama": "About us",
+    "heading": "About us",
+    "heading p": "Access Solutions is a small business with a special emphasis on providing high-quality services in the field of consulting and implementation of diverse solutions.",
+    "tko": "Who we are?",
+    "dugmic": "Learn more",
+    "usluge": "Our services ",
+    "tekstic1": "ICT Consulting involves expertly guiding organizations through the application of information technologies to enhance business and achieve goals. We provide tailored IT strategies and solutions that foster innovation and optimize technological resources.",
+    "tekstic2": "System and network integrations unify computer systems and network components to optimize an organization's technological infrastructure. This approach enhances functionality, communication, and overall efficiency.",
+    "tekstic3": "Cybersecurity involves safeguarding information systems from cyber threats, including protecting data, networks, and computer resources from unauthorized access, attacks, or damage. The primary goal is to preserve the security and integrity of digital information.",
+    "tekstic4": "Unified communication and collaboration refer to a coordinated approach to fostering effective communication and collaboration within an organization. This involves integrating various communication tools and collaboration platforms to enhance connectivity, streamline workflows, and promote a unified working environment.",
+    "tekstic5": "Infrastructure solutions encompass the implementation of technological infrastructure, including networks, servers, data storage systems, and other key components. Their goal is to support the business needs of the organization through reliable and efficient technological resources.",
+    "tekstic6": "Optimizing existing IT systems involves adapting and enhancing current information technology to achieve greater efficiency, improved performance, and reduced costs. This process includes analysis, identifying potential enhancements, and implementing strategies to maximize resources and enhance the overall operation of the organization's IT systems.",
+    "contact-infor": "Contact information",
+    "company": "Access Solutions is a company that provides consulting and implementation services and solutions in the domain of system and network integration, cybersecurity solutions, as well as unified communication and collaboration systems.",
+    "futer1": "About",
+    "futer2": "Services",
+    "futer3": "Contact",
+    "futer4": "Help",
+    "linkovi": "Links",
+    "futer5": "Contact"
       
     }
  };
@@ -93,7 +135,9 @@ enButton.addEventListener('click', () => {
 
 
 let menu = document.querySelector('#menu-btn');
-let navbarLinks = document.querySelector('.header .navbar .links .language-buttons .language-buttons1');
+let navbarLinks = document.querySelector('.header .navbar .links'); // Adjusted selector
+// Assuming language buttons are moved directly inside the navbar
+let languageButtonsContainer = document.querySelector('.header .navbar .language-buttons');
 
 menu.onclick = () =>{
    menu.classList.toggle('fa-times');
@@ -106,10 +150,11 @@ window.onscroll = () =>{
 
    if(window.scrollY > 60){
       document.querySelector('.header .navbar').classList.add('active');
-   }else{
+   } else {
       document.querySelector('.header .navbar').classList.remove('active');
    }
 }
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
@@ -123,5 +168,3 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
-
-
